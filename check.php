3757ca9cd746e7c926e5ac06b60af68613b6e829
Vinicius Vibrich <viniciusvibrich@gmail.com>
2020-03-09 16:18:05 +0000

    Adicionei bootstrap na página

--- check.php
+++ check.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Blacklist</title>
+</head>
+<body>
+ 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand center" href="#">Blacklist Check</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navig$
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+    </nav>
 <?php
 $bl = array(
 "bl.mailspike.net",
@@ -40,3 +57,6 @@ for($i = 0; $i <= 17; $i++){
 }
 
 ?>
+
+</body>
+</html>
